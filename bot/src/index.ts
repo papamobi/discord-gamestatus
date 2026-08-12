0ec5a09858f495d358a3544e01047cb8e59c28f7
papamobi <78706032+papamobi@users.noreply.github.com>
2026-08-12 14:47:59 +0100

    Update bot presence message

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -427,7 +427,7 @@ export default async function start(config: StartupConfig): Promise<Client> {
         activities: [
           {
             type: "WATCHING",
-            name: `game servers 🎮| /status or ${client.config.prefix}help`,
+            name: `game servers 🎮|  /status or ${client.config.prefix}help`,
           },
         ],
       });
